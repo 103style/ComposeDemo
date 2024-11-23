83c48992a1a4ba2795f4aa12345a3d4f5aab3031
kempluo <kempluo@tencent.com>
2024-11-23 21:52:09 +0800

    实现修改文本baseLine高度

---
+++ app/src/main/java/com/example/composedemo/chapter4_customlayout/CustomLayoutActivity.kt
@@ -0,0 +1,66 @@
+package com.example.composedemo.chapter4_customlayout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.composedemo.ui.theme.ComposeDemoTheme
+
+class CustomLayoutActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeDemoTheme {
+                Box(Modifier.safeDrawingPadding()) {
+                    TextWithPaddingToBaseLine()
+                }
+            }
+        }
+    }
+}
+
+/**
+ * 修改文本的firstBaseline
+ */
+fun Modifier.firstBaselineToTop(firstBaselineToTop: Dp) =
+    this.then(layout { measurable, constraints ->
+        // 测量元素
+        val placeable = measurable.measure(constraints)
+        // 获取元素基线值
+        val firstBaseline = placeable[FirstBaseline]
+        // 元素新的Y坐标 传入的baselineToTop - lineToTop
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        // 元素的高度
+        val height = placeable.height + placeableY
+
+        // 设置View的位置
+        layout(placeable.width, height) {
+            // 设置文本元素位置
+            placeable.placeRelative(0, placeableY)
+        }
+    })
+
+/**
+ * firstBaselineToTop 实现修改文本的baseLine高度
+ */
+@Composable
+fun TextWithPaddingToBaseLine() {
+    ComposeDemoTheme {
+        Text(
+            text = "let's do it.",
+            Modifier
+                .firstBaselineToTop(36.dp)
+                .background(Color.Red)
+        )
+    }
+}
