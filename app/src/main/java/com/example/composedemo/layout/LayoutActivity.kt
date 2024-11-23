e6a252a37a48b79dde20cba965080a0a1a002237
kempluo <kempluo@tencent.com>
2024-11-23 16:20:58 +0800

    update sdk version

--- app/src/main/java/com/example/composedemo/layout/LayoutActivity.kt
+++ app/src/main/java/com/example/composedemo/layout/LayoutActivity.kt
@@ -1,7 +1,18 @@
-package com.example.composedemo
+package com.example.composedemo.layout
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.composedemo.ui.theme.ComposeDemoTheme
 
 class LayoutActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeDemoTheme {
+                PhotographerCard()
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/example/composedemo/layout/PhotographerCard.kt
@@ -0,0 +1,71 @@
+package com.example.composedemo.layout
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composedemo.R
+
+/**
+ * ⚠️⚠️⚠️
+ * · Compose 的 Modifier 默认有先后顺序
+ * ⚠️⚠️⚠️
+ */
+@Composable
+fun PhotographerCard(modify: Modifier = Modifier) {
+    Row(
+        modifier = Modifier
+            .clip(RoundedCornerShape(4.dp))
+            .background(color = MaterialTheme.colorScheme.surface)
+            .clickable(onClick = {})
+            .padding(16.dp)
+    ) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.user_avatar), contentDescription = null
+            )
+        }
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text(text = "Ponny Ma", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentColor provides MaterialTheme.colorScheme.onSurfaceVariant) {
+                Text(
+                    text = "the owner or Tencent company.",
+                    style = MaterialTheme.typography.bodyMedium,
+//                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreView() {
+    PhotographerCard()
+}
