416013e953f7cd61e67457f735591b5e03a0d18f
kempluo <kempluo@tencent.com>
2024-11-23 17:25:05 +0800

    scaffold demo

--- app/src/main/java/com/example/composedemo/layout/LayoutActivity.kt
+++ app/src/main/java/com/example/composedemo/layout/LayoutActivity.kt
@@ -3,7 +3,6 @@ package com.example.composedemo.layout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import com.example.composedemo.ui.theme.ComposeDemoTheme
 
 class LayoutActivity : ComponentActivity() {
---
+++ app/src/main/java/com/example/composedemo/slotsapi/SlotsApiActivity.kt
@@ -0,0 +1,50 @@
+package com.example.composedemo.slotsapi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+class SlotsApiActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ScaffoldDemo()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScaffoldDemo() {
+    Scaffold(topBar = {
+        TopAppBar(title = { Text(text = "ScaffoldDemo") }, actions = {
+            IconButton(onClick = {}) {
+                Icon(imageVector = Icons.Filled.Favorite, contentDescription = null)
+            }
+        })
+    }) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding))
+    }
+
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(8.dp)) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Scaffold Study.")
+    }
+}
