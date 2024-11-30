c4dd96842feb04ac25cc0b734209b608f9d524e5
kempluo <kempluo@tencent.com>
2024-11-30 14:53:20 +0800

    effect api base code

---
+++ app/src/main/java/com/example/composedemo/chapter10_effect_api/EffectApiDemoActivity.kt
@@ -0,0 +1,28 @@
+package com.example.composedemo.chapter10_effect_api
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.ui.Modifier
+import com.example.composedemo.ui.theme.ComposeDemoTheme
+
+/**
+ * @author Created by kempluo 2024/11/30 14:51
+ */
+class EffectApiDemoActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        setContent {
+            ComposeDemoTheme {
+                Box(Modifier.safeDrawingPadding()) {
+
+                }
+            }
+        }
+    }
+}
